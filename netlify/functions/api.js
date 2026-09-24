5a0f55819050898b0a2f341841af145430ed4f13
Maxime <jordanebardella1@gmail.com>
2026-09-24 21:30:31 +0200

    Fix Netlify Express API

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,4 +1,20 @@
+const express = require('express');
 const serverless = require('serverless-http');
-const app = require('../../server');
+const cors = require('cors');
+
+const auth = require('../../api/auth');
+const formations = require('../../api/formations');
+const payment = require('../../api/payment');
+const content = require('../../api/content');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', auth);
+app.use('/api/formations', formations);
+app.use('/api/payment', payment);
+app.use('/api/content', content);
 
 module.exports.handler = serverless(app);
